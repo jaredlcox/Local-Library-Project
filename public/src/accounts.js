1cb0199f4b92fa782ed4daaf8e0e20ea279ed1e5
Jared Cox <96136301+jaredlcox@users.noreply.github.com>
2022-05-16 12:22:10 -0400

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,39 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+//   It returns the account object that has the matching ID.
+  return accounts.find((account) => account.id === id);
+//  here I am using .find to loop through the accounts array and creating an account function that "finds" an account.id that matches the id input by the user;
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+//   It returns a sorted array of the provided account objects. The objects are sorted alphabetically by last name.
+  return accounts.sort((accountA, accountB) => {
+    const lastNameA = accountA.name.last;
+    const lastNameB = accountB.name.last;
+    return lastNameA.toLowerCase() < lastNameB.toLowerCase() ? -1: 1;
+  });
+}
 
-function getTotalNumberOfBorrows(account, books) {}
+function getTotalNumberOfBorrows(account, books) {
+//   It returns a _number_ that represents the number of times the account's ID appears in any book's `borrows` array.
+  const result = books.reduce((acc, book) => {
+    let count = book.borrows.filter((b) => b.id === account.id).length;
+    acc +=count;
+    return acc;
+  }, 0)
+  return result;
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getBooksPossessedByAccount(account, books, authors) {
+  const accountId = account.id;
+  const booksBorrowed = books.filter((book) => {
+    return book.borrows[0].id === accountId && !book.borrows[0].returned
+  });
+  booksBorrowed.forEach((book) => {
+    const foundAuthor = authors.find((author) => author.id === book.authorId);
+    book.author = foundAuthor;
+  })
+  return booksBorrowed;
+}
 
 module.exports = {
   findAccountById,
